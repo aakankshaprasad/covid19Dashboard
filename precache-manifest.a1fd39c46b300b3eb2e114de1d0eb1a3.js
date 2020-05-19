f9d0b35bd015a2300975fc96749f7bdceab71601
Aakanksha Prasad <aakankshaprasad5@gmail.com>
2020-05-19 14:53:15 -0400

    Updates

---
+++ precache-manifest.a1fd39c46b300b3eb2e114de1d0eb1a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e6f5552e846fbe4660365931f6102",
+    "url": "/covid19Dashboard/index.html"
+  },
+  {
+    "revision": "bf182bc61fb11c9d5958",
+    "url": "/covid19Dashboard/static/css/main.6701cf04.chunk.css"
+  },
+  {
+    "revision": "b89da8546099d8b942ea",
+    "url": "/covid19Dashboard/static/js/2.1664f9a7.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid19Dashboard/static/js/2.1664f9a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf182bc61fb11c9d5958",
+    "url": "/covid19Dashboard/static/js/main.b1cdf9ad.chunk.js"
+  },
+  {
+    "revision": "60707eff4c19f0b1f776",
+    "url": "/covid19Dashboard/static/js/runtime-main.e6e7bdc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19Dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
